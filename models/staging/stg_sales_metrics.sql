53341f6176ea3b75fa8e891da0e67391edd0582d
ssanchez@qualesgroup.com <ssanchez@qualesgroup.com>
2026-08-26 17:29:38 +0000

    rename bronze

--- models/staging/stg_sales_metrics.sql
+++ models/staging/stg_sales_metrics.sql
@@ -1,11 +1,12 @@
 select
-    deal_id,
-    customer_name,
-    deal_value,
-    close_date,
-    sales_stage,
-    win_status,
-    sales_rep,
-    product_line,
-    loaded_at
+    deal_id as deal_id_src,
+    customer_name as customer_name_src,
+    deal_value as deal_value_src,
+    close_date as close_date_src,
+    sales_stage as sales_stage_src,
+    win_status as win_status_src,
+    sales_rep as sales_rep_src,
+    product_line as product_line_src,
+    loaded_at as loaded_at_src
+
 from {{ source('sales_intelligence', 'SALES_METRICS') }}
